442051f6b35981ed735a83744caa805bed6a5150
roselygirl2020 <pitchanan.kongarun@gmail.com>
2025-12-06 15:54:44 +0700

    eiei

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log
+const image_element=document.getElementById("school")
+console.log(image_element)
+
+let state = 1;
+image_element.addEventListener("click",()=> {
+    if(state == 1) {
+        image_element.src="./school1.jpg"
+        state = 2
+    }
+    else {
+        image_element.src="./SKR.png"
+        state = 1
+    }
+})
